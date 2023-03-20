b9dfb592494041e258f4b85513a36afbcb719bdb
Seren_Modz 21 <seren@kings-world.net>
2023-03-20 00:50:09 +0000

    feat: add etag header and cache /:key responses
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import { Hono, type MiddlewareHandler } from "hono";
+import { cache } from "hono/cache";
 import { nanoid } from "nanoid";
 
-type Env = {
-    MY_BUCKET: R2Bucket;
-    CDN_USERS: KVNamespace;
+type Options = {
+    Bindings: {
+        MY_BUCKET: R2Bucket;
+        CDN_USERS: KVNamespace;
+    };
+    Variables: {
+        user: string;
+    };
 };
 
-const app = new Hono<{ Bindings: Env }>();
-
+const app = new Hono<Options>();
 const cacheControl = "public, max-age=31536000"; // 1 year
-
-app.use(errorMiddleware());
 
 app.get("/", (c) => {
     return c.redirect("https://www.serenmodz.rocks", 301);
 });
 
-app.get("/:key", async (c) => {
+app.get("/:key", cache({ cacheName: "cdn:images", cacheControl }), async (c) => {
     const key = c.req.param("key");
 
     const object = await c.env.MY_BUCKET.get(key);
@@ -26,8 +29,9 @@ app.get("/:key", async (c) => {
     const contentType = object.httpMetadata?.contentType || "";
 
     return c.body(data, 200, {
-        "Cache-Control": cacheControl,
+        // "Cache-Control": cacheControl,
         "Content-Type": contentType,
+        ETag: object.httpEtag,
     });
 });
 
@@ -52,7 +56,11 @@ app.post("/upload", authMiddleware(), async (c) => {
         httpMetadata: {
             contentType: image.type,
             cacheControl: cacheControl,
-        }
+        },
+        customMetadata: {
+            "Uploaded-By": c.get("user"),
+            Domain: url.hostname,
+        },
     });
 
     return c.json({
@@ -78,21 +86,10 @@ function idLenth(query: string | undefined, def: number) {
     return id;
 }
 
-function errorMiddleware(): MiddlewareHandler<{ Bindings: Env }, never, {}> {
-    return async (c, next) => {
-        try {
-            await next();
-        } catch (e) {
-            const error = e instanceof Error ? e.message : e;
-            return c.json({ success: false, error }, 500);
-        }
-    }
-}
-
-function authMiddleware(): MiddlewareHandler<{ Bindings: Env }> {
+function authMiddleware(): MiddlewareHandler<Options> {
     return async (c, next) => {
         const missingAccess = () =>
-        c.json({ success: false, error: "Missing Access" }, 401);
+            c.json({ success: false, error: "Missing Access" }, 401);
 
         const header = c.req.header("Access-Token");
         if (!header) return missingAccess();
@@ -100,6 +97,8 @@ function authMiddleware(): MiddlewareHandler<{ Bindings: Env }> {
         const user = await c.env.CDN_USERS.get(header);
         if (!user) return missingAccess();
 
+        c.set("user", user);
+
         return next();
-    }
+    };
 }
