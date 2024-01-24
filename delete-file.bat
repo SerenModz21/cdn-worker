6f5137886ec37bd182e0345d1b14ed85a1063822
Seren_Modz 21 <seren@kings-world.net>
2024-01-24 23:35:30 +0000

    chore: remove dev scripts
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- delete-file.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-for %%x in (%*) do (
-    curl -sX DELETE -H "Access-Token: %CDN_ACCESS_TOKEN%" %%x
-    echo:
-)
-
-pause
--- delete-file.sh
+++
@@ -1,4 +0,0 @@
-for url in "$@"
-do
-    curl -sX DELETE -H "Access-Token: $CDN_ACCESS_TOKEN" $url
-done
